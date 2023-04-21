e2b63bae288f46b6c2dc7b52fd40f9d27406f07b
Gorle Ganesh <ganesh1gorle@gmail.com>
2023-04-21 10:42:54 +0530

    the final version of the code

--- test4.py
+++ test4.py
@@ -1,4 +1,6 @@
-count=0
-while count<3:
-    print("ganesh")
-    count=count+1
+for i in range(1,10):
+    print(i,end=" ")
+sum=0
+for i in range(1,10):
+    sum=sum+i
+    print("sum of numbers:",sum)
